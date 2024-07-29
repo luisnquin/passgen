57f1bdca7ac7155152cab9a0dbd90e4dd854c8fd
Luis Quiñones <lpaandres2020@gmail.com>
2024-07-28 19:18:07 -0600

    feat: more simple user api + bye cobra + 32 length by default

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"github.com/pedr0rocha/password-generator-cli/commands"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pedr0rocha/password-generator-cli/password"
 )
 
 func main() {
-	commands.Execute()
+	passLength, hasSymbols := 32, true
+
+	flag.IntVar(&passLength, "length", passLength, "Password length")
+	flag.IntVar(&passLength, "l", passLength, "Shorthand for --length")
+	flag.BoolVar(&hasSymbols, "symbols", hasSymbols, "Indicates if the password should contain symbols")
+	flag.BoolVar(&hasSymbols, "s", hasSymbols, "Shorthand for --symbols")
+	flag.Parse()
+
+	result, err := password.Generate(passLength, hasSymbols)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s\n", result)
 }
